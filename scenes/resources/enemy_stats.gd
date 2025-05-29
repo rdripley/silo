51803b45ae0dca2fc6dc6f67919b5f85ef005aba
rdripley <russellripley@hotmail.com>
2025-05-28 19:55:14 -0700

    Added folder colors. Added stats resources

---
+++ scenes/resources/enemy_stats.gd
@@ -0,0 +1,6 @@
+class_name EnemyStats
+extends Resource
+
+@export var knockback_power: int = 300
+@export var SPEED: float = 400.0
+@export var JUMP_VELOCITY: float = -400.0
---
+++ scenes/resources/weapon_stats.gd
@@ -0,0 +1,9 @@
+class_name GunStats
+extends Resource
+
+
+@export var shoot_speed: float
+@export var reload_speed: float
+@export var num_bullets: int
+@export var damage: float
+@export var weapon_texture: Texture
